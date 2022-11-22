ae97d7afbfa9063cb5aad44a8e4e854d6298f68c
Marc <mreta0002@gmail.com>
2022-11-21 20:19:27 -0800

    finished part 2 explore

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Marc Reta
 
 1) Number 2 would be the best fit for the automated tests in my Recipe project development pipeline since it will allow the developer to run tests after completing a short amount of code to see if it works. Once it works, they are able to push it to github.
 2) Yes
+3) I would use a unit test to test the “message” feature of a messaging application because the message feature is a very important part of the application. Unit Testing will allow us to test the feature for a wide variety of scenarios.
+4) I wouldn't use a unit test to test the “max message length” feature of a messaging application since there will be a lot of moving parts in the application. I believe it would be much easier to test this feature manually. 
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,123 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+
+test('isPhoneNumber: true', () => {
+
+    expect(functions.isPhoneNumber("(213) 327-7685")).toBe(true);
+
+});
+
+test('isPhoneNumber: true', () => {
+
+    expect(functions.isPhoneNumber("(213) 327-7686")).toBe(true);
+
+});
+
+test('isPhoneNumber: false', () => {
+
+    expect(functions.isPhoneNumber(2)).toBe(false);
+
+});
+
+test('isPhoneNumber: false', () => {
+
+    expect(functions.isPhoneNumber(1)).toBe(false);
+
+});
+
+test('isEmail: true', () => {
+
+    expect(functions.isEmail("mreta@ucsd.edu")).toBe(true);
+
+});
+
+test('isEmail: true', () => {
+
+    expect(functions.isEmail("mceta@ucsd.edu")).toBe(true);
+
+});
+
+test('isEmail: false', () => {
+
+    expect(functions.isEmail(1)).toBe(false);
+
+});
+
+test('isEmail: false', () => {
+
+    expect(functions.isEmail(2)).toBe(false);
+
+});
+
+test('isStrongPassword: true', () => {
+
+    expect(functions.isStrongPassword("Marcreta")).toBe(true);
+
+});
+
+test('isStrongPassword: true', () => {
+
+    expect(functions.isStrongPassword("Marcisawesome")).toBe(true);
+
+});
+
+test('isStrongPassword: false', () => {
+
+    expect(functions.isStrongPassword("2")).toBe(false);
+
+});
+
+test('isStrongPassword: false', () => {
+
+    expect(functions.isStrongPassword("5")).toBe(false);
+
+});
+
+test('isDate: true', () => {
+
+    expect(functions.isDate("2/26/2002")).toBe(true);
+
+});
+
+test('isDate: true', () => {
+
+    expect(functions.isDate("4/25/2022")).toBe(true);
+
+});
+
+test('isDate: false', () => {
+
+    expect(functions.isDate("2")).toBe(false);
+
+});
+
+test('isDate: false', () => {
+
+    expect(functions.isDate("4")).toBe(false);
+
+});
+
+test('isHexColor: true', () => {
+
+    expect(functions.isHexColor("#e34545")).toBe(true);
+
+});
+
+test('isHexColor: true', () => {
+
+    expect(functions.isHexColor("#e34546")).toBe(true);
+
+});
+
+test('isHexColor: false', () => {
+
+    expect(functions.isHexColor("2")).toBe(false);
+
+});
+
+test('isHexColor: false', () => {
+
+    expect(functions.isHexColor("3")).toBe(false);
+
+});
